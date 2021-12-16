f8893c033856f37da6d4be1b4fc4d1ea3d45b4a7
Jiping.Liu <jiping.liu@compass.com>
2021-12-16 22:10:47 +0800

    add map reduce

---
+++ examples/map-reduce.rs
@@ -0,0 +1,38 @@
+extern crate rayon;
+use rayon::prelude::*;
+
+struct Person {
+  age: u32,
+}
+
+fn main() {
+  let v = vec![
+    Person { age: 23 },
+    Person { age: 19 },
+    Person { age: 42 },
+    Person { age: 17 },
+    Person { age: 17 },
+    Person { age: 31 },
+    Person { age: 30 },
+  ];
+
+  let num_over_30 = v.par_iter().filter(|&x| x.age > 30).count() as f32;
+  let sum_over_30 = v.par_iter()
+    .map(|x| x.age)
+    .filter(|&x| x > 30)
+    .reduce(|| 0, |x, y| x + y);
+  println!("num_over_30: {}, sum_over_30: {}", num_over_30, sum_over_30);
+
+  
+  let alt_sum_30: u32 = v.par_iter()
+    .map(|x| x.age)
+    .filter(|&x| x > 30)
+    .sum();  
+  println!("alt_sum_30:{}", alt_sum_30);
+
+  let avg_over_30 = sum_over_30 as f32/num_over_30;
+  let alt_avg_over_30 = alt_sum_30 as f32/num_over_30;
+
+  assert!((avg_over_30 - alt_avg_over_30).abs() < std::f32::EPSILON);
+  println!("The average age of people older than 30 is {}", avg_over_30);
+}
